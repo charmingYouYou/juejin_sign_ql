cff142eef8d48ff263e8dcc034a206b885e3a276
Orange <huangkaiking@126.com>
2021-08-14 11:57:38 +0800

    去掉日期显示

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ if (!COOKIE) {
 } else {
   const api = require('./lib/api')(COOKIE)
   api.check_in().then((data1) => {
-    let date = new Date()
-    message(
-      `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} 签到成功`,
-    )
+    message(`签到成功`)
     api.draw().then((data2) => {
       message(`抽奖成功，获得：${data2.lottery_name}`)
     })
