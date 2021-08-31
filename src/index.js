8613ad5bafd088aabd4e4b54c4b8eddad5753630
寇超 <kouchao@xindong.com>
2021-08-31 11:10:34 +0800

    feat: 抽奖梭哈

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
-const { COOKIE } = require('./lib/config')
+const { COOKIE, all_in } = require('./lib/config')
 const message = require('./lib/message')
 
 if (!COOKIE) {
   message('获取不到cookie，请检查设置')
 } else {
   const api = require('./lib/api')(COOKIE)
-  api.check_in().then((data1) => {
+
+  // 获取可抽奖次数
+  async function get_raw_time(){
+    const res = await api.get_cur_point()
+    return Math.floor(res / 200)
+  }
+
+  // 抽奖一次
+  async function draw() {
+    const res = await api.draw()
+    message(`抽奖成功，获得：${res.lottery_name}`)
+    return res
+  }
+
+  // 抽所有
+  async function draw_all(){
+    const time = await get_raw_time()
+    message(`梭哈, 可抽奖次数${time}`)
+    if(!time){
+      message(`抽奖完成`)
+    }
+
+    for(let i = 0; i < time; i++) {
+      await draw()
+    }
+
+    if(await get_raw_time()){
+      draw_all()
+    }
+  }
+
+  api.check_in().then(() => {
     message(`签到成功`)
-    api.draw().then((data2) => {
-      message(`抽奖成功，获得：${data2.lottery_name}`)
-    })
+    if(all_in){
+      draw_all()
+      return
+    }
+    draw()
   })
 }
--- src/lib/config.js
+++ src/lib/config.js
@@ -2,4 +2,5 @@ module.exports = {
   COOKIE: process.env.COOKIE || '',
   DINGTALK_WEBHOOK: process.env.DINGTALK_WEBHOOK || '',
   DINGTALK_SECRET: process.env.DINGTALK_SECRET || '',
+  all_in: false,
 }
