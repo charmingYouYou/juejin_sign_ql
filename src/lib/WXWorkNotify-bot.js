e78ba219683fe616677ed128767da75a09f56362
何伟峰 <954995880@qq.com>
2021-10-08 15:59:21 +0800

    Update WXWorkNotify-bot.js

--- src/lib/WXWorkNotify-bot.js
+++ src/lib/WXWorkNotify-bot.js
@@ -4,21 +4,25 @@ const config = require('./config.js')
 let msg = ''
 let timer = ''
 module.exports = function bot(message) {
-  msg += message
-  
-  timer && clearTimeout(timer)
-  timer = setTimeout(function(){
-    WXWorkNotify({
-        id: config.WX_COMPANY_ID, // 企业 ID
-        agentId: config.WX_APP_ID, // 应用 ID
-        secret: config.WX_APP_SECRET, // 应用 secret
-        msgData: {
-          msgtype: "text",
-          text: {
-            content: msg
+  if(config.WX_COMPANY_ID && config.WX_APP_ID && config.WX_APP_SECRET) {
+    
+    msg += message
+
+    timer && clearTimeout(timer)
+    timer = setTimeout(function(){
+      WXWorkNotify({
+          id: config.WX_COMPANY_ID, // 企业 ID
+          agentId: config.WX_APP_ID, // 应用 ID
+          secret: config.WX_APP_SECRET, // 应用 secret
+          msgData: {
+            msgtype: "text",
+            text: {
+              content: msg
+            }
           }
-        }
-      });
-  }, 500)
+        });
+    }, 500)
+    
+  }
     
 }
