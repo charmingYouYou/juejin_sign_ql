c07f2bf9518d6f897ed816b2f9e875fe2293c793
Tassel <tasselx@users.noreply.github.com>
2021-10-28 18:11:31 +0800

    添加飞书通知

--- src/lib/config.js
+++ src/lib/config.js
@@ -6,4 +6,6 @@ module.exports = {
   WX_COMPANY_ID: process.env.WX_COMPANY_ID || '', // 企业 ID
   WX_APP_ID: process.env.WX_APP_ID || '', // 应用 ID
   WX_APP_SECRET: process.env.WX_APP_SECRET || '', // 应用 secret
+  FEISHU_WEBHOOK: process.env.FEISHU_WEBHOOK || '',
+  FEISHU_SECRET: process.env.FEISHU_SECRET || '',
 }
